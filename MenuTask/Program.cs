febfbb79426a8fef8edc2427de67dd0f8663611b
KawsarHussain <kawsar.hussain.365@gmail.com>
2024-03-01 13:15:19 +0000

    Truncated the code to use a method for inputting

--- MenuTask/Program.cs
+++ MenuTask/Program.cs
@@ -6,37 +6,38 @@ Once logged in, a menu should be presented. Once the menu item has been selected
 */
 
 //Set up username and password
+
 string username = "user";
 string password = "Password123";
 
-//Inputting of username
-Console.Write("Username: ");
 string inputUsername;
-//does not allow user to skip input
-while (true)
+string inputPassword;
+
+inputUsername = InputCredentials("Username");
+
+inputPassword = InputCredentials("Password");
+
+
+//Method allows for the input of credentials
+string InputCredentials(string inputTerm)
 {
-    inputUsername = Console.ReadLine();
-    if (inputUsername == "")
-        Console.Write("""
-            Please input a Username
-            Username: 
+    //Inputting of Whatever is needed
+    Console.Write($"{inputTerm}: ");
+    string inputIn;
+    //does not allow user to skip input
+    while (true)
+    {
+        inputIn = Console.ReadLine();
+        if (inputIn == "")
+            Console.Write($"""
+            Please input a {inputTerm}
+            {inputTerm}: 
             """);
-    else
-        break; 
+        else
+            break;
+    }
+    return inputIn;
 }
 
-//Inputting of password
-Console.Write("Password: ");
-string inputPassword;
-//Does not allow user to skip input
-while (true)
-{
-    inputPassword = Console.ReadLine();
-    if (inputPassword == "")
-        Console.Write("""
-            Please input a Password
-            Password: 
-            """);
-    else
-        break;
-}
+
+
