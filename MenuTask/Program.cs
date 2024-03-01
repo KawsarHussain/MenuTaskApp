e9dbc2c597570d4777d38d54c5d88b7361e8e1b6
KawsarHussain <kawsar.hussain.365@gmail.com>
2024-03-01 13:49:06 +0000

    Made the Program neater

--- MenuTask/Program.cs
+++ MenuTask/Program.cs
@@ -14,44 +14,36 @@ string inputUsername = InputCredentials("Username");
 string inputPassword = InputCredentials("Password");
 
 //Checks to see if the user inputs were correct
-while (true)
+while (!(string.Equals(inputUsername, username, StringComparison.OrdinalIgnoreCase) && string.Equals(inputPassword, password)))
 {
-    if (string.Equals(inputUsername, username, StringComparison.OrdinalIgnoreCase) && string.Equals(inputPassword, password))
-    {
-        Console.WriteLine("""
-        You have inputted the correct password and username.
+    //Incorrect Credentials
+    Console.Clear();   
+    Console.WriteLine("You have inputted the wrong username or password. Try again");
+    inputUsername = InputCredentials("Username");
+    inputPassword = InputCredentials("Password");
+}
+Console.Clear();
+Console.WriteLine("""
+        You have inputted the correct username and password.
         You may now proceed onto the menu.
         Hope you will have a wonderful time exploring our options
         """);
-        break;
-    }
-    else
-    {
-        Console.WriteLine("You have inputted the wrong username or password. Try again");
-        inputUsername = InputCredentials("Username");
-        inputPassword = InputCredentials("Password");
-
-    }
-}
 
 
 //Method allows for the input of credentials
 string InputCredentials(string inputTerm)
 {
-    //Inputting of Whatever is needed
     Console.Write($"{inputTerm}: ");
-    string inputIn;
+    string inputIn = Console.ReadLine();
+    
     //does not allow user to skip input
-    while (true)
+    while (inputIn == "")
     {
+        Console.Write($"""
+         Please input a {inputTerm}
+         {inputTerm}: 
+         """);
         inputIn = Console.ReadLine();
-        if (inputIn == "")
-            Console.Write($"""
-            Please input a {inputTerm}
-            {inputTerm}: 
-            """);
-        else
-            break;
     }
     return inputIn;
 }
