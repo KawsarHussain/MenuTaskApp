1b273845331447e77cc8d372621a8c90e04a7000
KawsarHussain <kawsar.hussain.365@gmail.com>
2024-03-01 11:58:44 +0000

    Add project files.

---
+++ MenuTask/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Create a menu where a user would have to log in to with a username and password
+Username shouldn't be case specific
+Passwords should be case specific
+Once logged in, a menu should be presented. Once the menu item has been selected, just repeat the menu selection back
+*/
+
+//Set up username and password
+string username = "user";
+string password = "Password123";
+
+//Inputting of username and password
+Console.Write("Username: ");
+string inputUsername;
+while (true)
+{
+    inputUsername = Console.ReadLine();
+    if (inputUsername == "")
+        Console.Write("""
+            Please input a Username
+            Username: 
+            """);
+    else
+        break; 
+}
+
+Console.Write("Password: ");
+String inputPassword = Console.ReadLine();
+
